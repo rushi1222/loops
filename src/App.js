ae0cc1acff016a68ef9a8835cdf22690aa1e196b
rushi1222 <k.rushi1222@gmail.com>
2022-04-30 14:09:51 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,57 +1,50 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let table  = []
-
-///for loop
-  // for (let i = 1; i <= 100; i++) { ///first iteration
-  //   let t = [];
-  //   for (let j = 1; j <= 10; j ++){ ///second iteration
-  //     if (i === 1 || i===2 || i ===3 || i ===5 || i === 9 || i === 15 || i === 25|| i === 45 || i === 67 || i === 89 || i === 96 ) {
-  //   break;
-  //   }
-  //  if( (i*j)%44 === 0 || (i*j)%55 === 0 || (i*j)%66 === 0 || (i*j)%33 === 0 || (i*j)%21 === 0 || (i*j)%49 === 0 || (i*j)%69 === 0 || (i*j)%83 === 0 || (i*j)%97 === 0 ){
-  //   break;
-  //  }
-  //  t.push(`${i}*${j} = ${i*j}`)
-  //      }
-  //    table.push(t)
-  // }
-
-///// while
-let tableWhile = [];
-let i = 1; 
 
 
-while(i <= 5){
-    let j = 1;
-    let t = [];
-  while(j <= 5 ){
-  t.push(`${i}*${j} = ${i*j}`)
-    j++
-   
-    }
-    tableWhile.push(t)
-  
-  i++
- 
-  // console.log(t)
-  
+
+const [score,setScore] = useState(0);
+const [balls,setBalls] = useState(0);
+const [sixers,setSixers] = useState(0);
+const [fours,setFours] = useState(0);
+const [doubles,setDoubles] = useState(0);
+const [singles,setSingles] = useState(0);
+const [dotballs,setDotballs] = useState(0);
+const [wickets,setWickets] = useState(0);
+const [wides,setWides] = useState(0);
+
+const scoreSetting = (number)=>{
+setScore(score=> score+ number)
 }
-console.log(tableWhile)
-
-
-
-  
-
-
-
+const ballCount = ()=>{
+  setBalls(ball=> ball+1)
+}
   return (
+    
     <div className="App">
-    tables
+      <h1>Score:{score}</h1>
+      <h1>Balls:{balls}</h1>
+      <h1>Sixers:{sixers}</h1>
+      <h1>Fours:{fours}</h1>
+      <h1>Doubles:{doubles}</h1>
+      <h1>Singles:{singles}</h1>
+      <h1>Dot balls:{dotballs}</h1>
+      <h1>Wickets:{wickets}</h1>
+      <h1>Wides:{wides}</h1>
+   <button onClick = {()=>{setSixers(e =>(e+6) ); scoreSetting(6);ballCount()}}>Sixer</button>
+   <button onClick = {()=>{setFours(e =>(e+4) ); scoreSetting(4);ballCount()}}>Four</button>
+   <button onClick = {()=>{setDoubles(e =>(e+2) ); scoreSetting(2);ballCount()}}>doubls</button>
+   <button onClick = {()=>{setSingles(e =>(e+1) ); scoreSetting(1);ballCount()}}>single</button>
+   <button onClick = {()=>{setDotballs(e=>(e+1));ballCount()}}>Dotballs</button>
+   <button onClick = {()=>{setWickets(e=>(e+1));ballCount()}}>Wicket</button>
+   <button onClick = {()=>{setWides(e=>(e+1));scoreSetting(1)}}>Wide</button>
+
     </div>
+    
   );
 }
 
